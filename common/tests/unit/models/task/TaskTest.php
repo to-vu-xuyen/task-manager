926aec69d19c3f0adeac297bbc551047f5d7596c
To Vu Xuyen <tovuxuyen2@gmail.com>
2026-02-03 17:30:30 +0700

    test(task): update Task unit tests and test data

--- common/tests/unit/models/task/TaskTest.php
+++ common/tests/unit/models/task/TaskTest.php
@@ -2,10 +2,15 @@
 
 namespace common\tests\unit\models\task;
 
+use Yii;
 use Codeception\Test\Unit;
+
 use common\fixtures\task\TaskFixture;
 use common\fixtures\UserFixture;
 use common\models\task\Task;
+use common\dto\task\TaskDto;
+use common\services\task\TaskServiceInterface;
+use common\forms\task\TaskCreateForm;
 
 
 class TaskTest extends Unit
@@ -30,20 +35,88 @@ class TaskTest extends Unit
 
     public function testIsOverDue()
     {
-        $task = $this->tester->grabFixture('tasks', 'task_overdue_1');
-        $overdueTask = Task::findOne($task['id']);
-        $this->assertTrue($overdueTask->isOverdue());
+        $taskOverDue = $this->tester->grabFixture('tasks', 'task_overdue_1');
+        // $overdueTask = Task::findOne($task['id']);
+        $this->assertTrue($taskOverDue->isOverdue());
     }
 
-    // public function testTask()
-    // {
-    //     $task = new Task();
-    //     $task->title = 'Test Task';
-    //     $task->description = 'Test Description';
-    //     $task->status = 'pending';
-    //     $task->due_at = '2022-01-01';
-    //     $task->user_id = 1;
-    //     $task->assignee_id = 1;
-    //     $this->assertTrue($task->save());
-    // }
+    public function testIsNotOverDue()
+    {
+        $taskPending = $this->tester->grabFixture('tasks', 'task_pending_1');
+        $this->assertFalse($taskPending->isOverdue());
+    }
+
+    public function testIsCompleted()
+    {
+        $taskCompleted = $this->tester->grabFixture('tasks', 'task_completed_1');
+        $this->assertTrue($taskCompleted->isCompleted());
+    }
+
+    public function testIsNotCompleted()
+    {
+        $taskPending = $this->tester->grabFixture('tasks', 'task_pending_1');
+        $this->assertFalse($taskPending->isCompleted());
+    }
+
+    public function testValidationRequired()
+    {
+        $task = new Task();
+        $this->assertFalse($task->validate());
+        $this->assertArrayHasKey('title', $task->getErrors());
+        $this->assertArrayHasKey('user_id', $task->getErrors());
+    }
+
+    public function testSoftDelete(){
+        $task = $this->tester->grabFixture('tasks', 'task_pending_1');
+        $task->softDelete();
+        $this->assertNotNull($task->deleted_at);
+        $this->assertEquals(Task::STATUS_DELETED, $task->status);
+    }
+
+    public function testServiceSoftDelete(){
+        $task = $this->tester->grabFixture('tasks', 'task_pending_1');
+        $service = Yii::$container->get(TaskServiceInterface::class);
+        $service->delete($task->id);
+        $task = $service->getById($task->id);
+        $this->assertNotNull($task->deleted_at);
+        $this->assertEquals(Task::STATUS_DELETED, $task->status);
+    }
+
+    public function testIsSoftDelete(){
+        $task = $this->tester->grabFixture('tasks', 'task_deleted_1');
+        $this->assertNotNull($task->deleted_at);
+        $this->assertEquals(Task::STATUS_DELETED, $task->status);
+    }
+
+    public function testTaskUserRelation()
+    {
+        $task = $this->tester->grabFixture('tasks', 'task_pending_1');
+        $this->assertEquals('bayer.hudson', $task->user->username);
+    }
+
+    public function testSaveTask()
+    {
+        $taskDto = new TaskCreateForm();
+        $taskDto->title = 'New Task 1';
+        $taskDto->description = 'New Description 1';
+        $taskDto->content = 'New Content 1';
+        // $taskDto->status = Task::STATUS_PENDING;
+        $taskDto->due_at = '2028-01-01';
+        $taskDto->user_id = 1;
+        $taskDto->assignee_id = 1;
+
+        $service = Yii::$container->get(TaskServiceInterface::class);
+        $result = $service->create($taskDto);
+        
+        $this->assertInstanceOf(Task::class, $result);
+        $this->assertEquals($taskDto->title, $result->title);
+        $this->assertEquals($taskDto->description, $result->description);
+        $this->assertEquals($taskDto->content, $result->content);
+        $this->assertEquals($taskDto->status, $result->status);
+        $this->assertEquals($taskDto->due_at, $result->due_at);
+        $this->assertEquals($taskDto->user_id, $result->user_id);
+        $this->assertEquals($taskDto->assignee_id, $result->assignee_id);
+
+    }
+
 }
